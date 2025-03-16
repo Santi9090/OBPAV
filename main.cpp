62f9f0be8a9c702167d63c5e8954cc376b26bf4f
Santi9090 <114961199+Santi9090@users.noreply.github.com>
2025-03-16 17:16:16 -0300

    main.cpp y menu basico

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int menu=9; // Variable que almacena la opción del menú
+
+do
+{
+    cout << "Bienvenido!" << endl
+    cout << "Elija La Opcion:" << endl
+    cout << "    1)Registrar Socio" << endl
+    cout << "    2)Agregar Mascota" << endl
+    cout << "    3)Ingresar consulta" endl
+    cout << "    0)Salir" << endl
+switch (menu) // Estructura de control switch del menu
+{
+case menu == 1:
+    break;
+case menu == 2:
+    break;
+case menu == 3:
+break;
+case menu == 4:
+    break;
+default:
+    cout << "Opción no válida" << endl;
+    break;
+}
+
+
+} while (meun != 0); // Bucle que se ejecuta mientras la opción del menú sea diferente de 0
