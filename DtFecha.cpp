b3ffdae01476158fd17ff325fd0aa6a0c2336347
Santi9090 <114961199+Santi9090@users.noreply.github.com>
2025-03-16 17:30:10 -0300

    xd

--- DtFecha.cpp
+++ DtFecha.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 DtFecha::DtFecha() : dia(1), mes(1), anio(2000) {}
 DtFecha::DtFecha(int dia, int mes, int anio) : dia(dia), mes(mes), anio(anio) {}
+
 int DtFecha::getDia() const {
      return dia; 
     }
